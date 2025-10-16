f0b5f10562f0906e487593d622bba792b47f313e
Raihan C <139701695+raihanCarder@users.noreply.github.com>
2025-10-16 00:16:48 -0400

    Add Mobile Nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,28 @@
+import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
+import { log } from "console";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Raihan", lastName: "C" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
